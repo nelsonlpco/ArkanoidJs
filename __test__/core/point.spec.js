c73e6a74e21bf01dca7c145619485df307dee505
Nelson Oliveira <nelsonlpco@gmail.com>
2018-07-08 12:04:16 -0300

    add point class to projet

---
+++ __test__/core/point.spec.js
@@ -0,0 +1,29 @@
+import Point from '../../src/core/point';
+
+describe('Point', () => {
+  test('Should be make a point', () => {
+    const point = new Point(2, 3);
+    const point2 = new Point('2', '3');
+
+    expect(point.Point).toEqual({ x: 2, y: 3 });
+    expect(point.X).toEqual(2);
+    expect(point.Y).toEqual(3);
+
+    expect(point2.Point).toEqual({ x: 2, y: 3 });
+    expect(point2.X).toEqual(2);
+    expect(point2.Y).toEqual(3);
+  });
+
+  test('should be throws an error if the x point is not a number', () => {
+    const mockFunction = () => { new Point('a', 2); };
+
+    expect(mockFunction).toThrowError();
+  });
+
+  test('should be throws an error if the y point is not a number', () => {
+    const mockFunction = () => { new Point(2, 'a'); };
+
+    expect(mockFunction).toThrowError();
+  });
+
+});
---
+++ src/core/point.js
@@ -0,0 +1,35 @@
+class Point {
+  constructor(x, y) {
+    this.X = x;
+    this.Y = y;
+  }
+
+  set X(x) {
+    if (Number.isNaN(Number(x))) {
+      throw new Error(`${x} is not a number`);
+    }
+
+    this.x = Number(x);
+  }
+
+  get X() {
+    return this.x;
+  }
+
+  set Y(y) {
+    if (Number.isNaN(Number(y))) {
+      throw new Error(`${y} is not a number`);
+    }
+    this.y = Number(y);
+  }
+
+  get Y() {
+    return this.y;
+  }
+
+  get Point() {
+    return { x: this.X, y: this.Y };
+  }
+}
+
+export default Point;
